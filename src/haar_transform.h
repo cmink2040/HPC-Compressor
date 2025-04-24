f53f56df599fc2bd7536366c298fb0ecdf5ba893
Steve Zeng <cmink2040@gmail.com>
2025-04-24 15:47:31 -0500

    Inverse added + bracket reform

--- src/haar_transform.h
+++ src/haar_transform.h
@@ -1,14 +1,36 @@
-void haar_wavelet_transform(float* data, int n) {
+void haar_wavelet_transform(float* data, int n) 
+{
     float temp[n]; 
 
-    for (int i = 0; i < n; i += 2) {
+    for (int i = 0; i < n; i += 2) 
+    {
         float avg  = (data[i] + data[i + 1]) / 2.0f;
         float diff = (data[i] - data[i + 1]) / 2.0f;
         temp[i / 2] = avg;
         temp[n / 2 + i / 2] = diff;
     }
 
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++) 
+    {
+        data[i] = temp[i];
+    }
+}
+
+void haar_wavelet_inverse(float* data, int n) 
+{
+    float temp[n];  
+
+    for (int i = 0; i < n / 2; i++) 
+    {
+        float avg  = data[i];
+        float diff = data[n / 2 + i];
+        temp[2 * i]     = avg + diff;
+        temp[2 * i + 1] = avg - diff;
+    }
+
+    // Copy result back into data
+    for (int i = 0; i < n; i++) 
+    {
         data[i] = temp[i];
     }
 }
